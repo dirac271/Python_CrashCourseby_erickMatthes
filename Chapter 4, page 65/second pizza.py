62ae3152be27578501d362c8ec0afe6d862ea828
Dirac <122928674+dirac271@users.noreply.github.com>
2023-10-09 09:42:35 -0600

    Add files via upload

---
+++ Chapter 4, page 65/second pizza.py
@@ -0,0 +1,12 @@
+fav_pizzas = ['pineapple on pizza','vegetable pizzas', 'cheese pizza']
+fav_pizzas_friends = fav_pizzas[:]
+fav_pizzas_friends.append('churrasco pizza')
+
+for pizza in fav_pizzas:
+	print(f"{pizza.title()}, these are my favorite pizzas")
+print("\n")
+for pizza in fav_pizzas_friends:
+	print(f"{pizza.title()},these are my favorite pizzas")
+
+print("\npizza I REALLY LOVE PIZZA AND ENRICO FERMI")
+
