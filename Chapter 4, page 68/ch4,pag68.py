21405ba70c13e0072d383135ee694027949ae122
Dirac <122928674+dirac271@users.noreply.github.com>
2023-10-29 09:30:15 -0600

    Add files via upload

---
+++ Chapter 4, page 68/ch4,pag68.py
@@ -0,0 +1,6 @@
+dimensions = ("pizza","bistek vip","potato","drugs with eggs","rice and beans")
+for dimension in dimensions:
+    print(dimension)
+dimensions = ("\nconflakes","mariguana","potato","drugs with eggs","rice and beans")
+for dimension2 in dimensions:
+    print(dimension2)
