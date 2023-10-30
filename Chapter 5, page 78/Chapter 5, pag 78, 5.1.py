ed6f6b69008facb0be7ab8c15ade038923bdca7c
Dirac <122928674+dirac271@users.noreply.github.com>
2023-10-30 10:15:10 -0600

    Add files via upload

---
+++ Chapter 5, page 78/Chapter 5, pag 78, 5.1.py
@@ -0,0 +1,38 @@
+#Ex5.1
+phy1 = 'albert einstein'
+phy2 = 'Enrico Fermi'
+phy3 = 'Paul Dirac'
+phy4 = 'Louis de Broglie'
+phy5 = 'Oppenheimer'
+phy6 = 'Klaus Fiuch'
+phy7 = 'Von Neumann'
+phy8 = 'Hans Bete'
+phy9 = 'Heisenberg'
+
+print("Is the phycisist phy1 == Albert einstein? I predicted true") #1
+print(phy1 == 'albert einstein')
+
+print("Is the phycisist phy6 = 'Lorentz?' I predicted true" )#2
+print(phy6 == 'Lorentz')
+
+print("Is the phycisist = phy4 De broglie? i guess yes")#3
+print(phy4 == 'Louis de Broglie')
+
+print("Is the phycisist phy6 = 'Curie?' I predicted true" ) #4
+print(phy6 == 'Lorentz')
+
+print("Is the phycisist phy3 = 'Lorentz?' I predicted true" )#5
+print(phy3 == 'Paul Dirac')
+print("Is the phycisist phy3 = 'Opennheimer?' I predicted false" )#6
+print(phy3 == 'Paul Dirac')
+print("Is the phycisist phy8 = 'Lawrence?' I predicted false" )#7
+print(phy8 == 'Hans Bete')
+print("Is the phycisist phy6 = 'Feynman?' I predicted true" )#8
+print(phy6 == 'Lorentz')
+
+print("Is the phycisist phy9 = 'Neumann?' I predicted false" )#9
+print(phy9 == 'Heisenberg')
+
+print("Is the phycisist phy7 = 'Lawrence?' I predicted false" )#10
+print(phy7 == 'Von Neumann')
+
