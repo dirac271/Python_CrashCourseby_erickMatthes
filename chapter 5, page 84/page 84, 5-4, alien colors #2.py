b0c7c886de64d3e6ac1346b6a993e846ea140e33
Dirac <122928674+dirac271@users.noreply.github.com>
2023-11-03 19:27:01 -0600

    Add files via upload

---
+++ chapter 5, page 84/page 84, 5-4, alien colors #2.py
@@ -0,0 +1,6 @@
+#5-4. Alien Colors #2: 
+alien_color = 'green'
+if alien_color == 'green':
+    print("you have earned 5 points for shooting the alien")
+else:
+    print("you haven earned 10 points")
