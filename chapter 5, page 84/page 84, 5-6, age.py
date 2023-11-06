41b14ad2e865f05c17c86218e99c79b422fe2f18
Dirac <122928674+dirac271@users.noreply.github.com>
2023-11-05 19:10:09 -0600

    Add files via upload

---
+++ chapter 5, page 84/page 84, 5-6, age.py
@@ -0,0 +1,14 @@
+#5-6.
+age = 2
+if(age < 2):
+    print("You're a baby")
+elif(age < 4):
+    print("You're a toddler")
+elif(age < 13):
+    print("You're a kid")
+elif(age < 20):
+    print("You're a teenager")
+elif(age < 65):
+    print("You're an adult, i'm sorry bro you aren't a kid anymore")
+else:
+    print("You're an elder")
