6228946c506860b62009999fc0f735ac046bcfcf
Dirac <122928674+dirac271@users.noreply.github.com>
2023-11-05 19:23:34 -0600

    Add files via upload

---
+++ chapter 5, page 84/page 84, favorite fruit, 5-7.py
@@ -0,0 +1,12 @@
+#Favorite fruit,5-7
+favorite_fruits = ['orange','strawberry','banana',]
+if('orange' in favorite_fruits):
+    print("u really like orange ? right")
+if('watermelon' in favorite_fruits):
+    print("tfk bro you're insane, i like watermelon SUGAR")
+if('strawberry' in favorite_fruits):
+    print("STOP STOP BRO STRAWBERRY IS THE GOAT")
+if('mango' in favorite_fruits):
+    print("not bad but i disagree")
+if('banana' in favorite_fruits):
+    print("one of the best fruits in the universe")
