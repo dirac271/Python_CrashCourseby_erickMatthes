3021a7b4e030be789dabae3cc2aa3217526a7c42
Dirac <122928674+dirac271@users.noreply.github.com>
2023-11-07 17:45:20 -0600

    Add files via upload

---
+++ chapter 6, page 99/#6-1. Person.py
@@ -0,0 +1,6 @@
+#6-1. Person: 
+Dirac = {'first_name':'Paul','last_name':'Dirac','age':31,'City':'Bristol'}
+print(Dirac['last_name'])
+print(Dirac['age'])
+print(Dirac['City'])
+print(Dirac['first_name'])
