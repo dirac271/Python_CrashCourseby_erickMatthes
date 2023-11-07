2bb757b27173c7ea9f8b48c5e96b4460fc63ff90
Dirac <122928674+dirac271@users.noreply.github.com>
2023-11-06 19:07:52 -0600

    Add files via upload

---
+++ chapter 5/page 89/page 89,5-8. Hello Admin.py
@@ -0,0 +1,7 @@
+#5-8. Hello Admin:
+usernames = ['admin','user2','user69','user66','user1902']
+for username in usernames:
+    if(username == 'admin'):
+        print("hey admin, would u like to see a status report")
+    else:
+        print(f"hello {username} thank u for logging in again")
