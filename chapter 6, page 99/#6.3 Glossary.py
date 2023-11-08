19dce3efa4fdb12d428b5a7f8cd1a96301192a91
Dirac <122928674+dirac271@users.noreply.github.com>
2023-11-08 13:55:26 -0600

    Add files via upload

---
+++ chapter 6, page 99/#6.3 Glossary.py
@@ -0,0 +1,8 @@
+#6-3. Glossary:
+glossary = {'variable':'It stores information','Upper function':'It converts text information in upper-casse letter','lists':'it stores information about a specific set','Tuples':'Equals to lists, but the difference is that ',
+            'python':'A high-level programming language','Machine code':'The lowest level about information between the computer and code'}
+print(f"Variable : {glossary['variable']}")
+print(f"Upper function : {glossary['Upper function']}")
+print(f"Python : {glossary['python']}")
+print(f"Machine Code : {glossary['Machine code']}")
+print(f"Lists : {glossary['lists']}")
