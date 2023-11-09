c6bf21b320979a5315bb44302280394b098fd4c9
Dirac <122928674+dirac271@users.noreply.github.com>
2023-11-08 18:04:40 -0600

    Add files via upload

---
+++ chapter 6, page 105/#6.4 Glossary 2.py
@@ -0,0 +1,8 @@
+#6-4. Glossary 2:
+glossary = {'variable':'It stores information','Upper function':'It converts text information in upper-casse letter','lists':'it stores information about a specific set','Tuples':'Equals to lists, but the difference is that ',
+            'python':'A high-level programming language','Machine code':'The lowest level about information between the computer and code','dictionaries':'They are like lists, but they can store multiple values of information',
+            'Sort() method':'It organize any item in alphabetical order','Append() method':'It adds and stores information about something to a list, a string commonly','String':'A chain of characteres together in a set','Constant'
+            :'A value that never changes his size or value.'}
+for word, definition in glossary.items():
+    print(f"\nWord: {word}")
+    print(f"Definition: {definition}")
