4dbe527b223423331266c06a78dd1fe8ac268865
Dirac <122928674+dirac271@users.noreply.github.com>
2023-11-10 14:26:21 -0600

    Add files via upload

---
+++ chapter 6, page 112/6-8 pets.py
@@ -0,0 +1,10 @@
+#6-8. Pets:
+Obito = {'kind':'Dog','Owner':'Alejandro','Name':'Obito'}
+Clifor = {'kind':'Cat','Owner':'Josue','Name':'Flower'}
+Fredy = {'kind':'Bear','Owner':'Ozziel','Name':'Fredy'}
+pets = [Obito,Clifor,Fredy]
+for pet in pets:
+    print("\nKind of animal:", pet['kind'])
+    print("Owner's Name:", pet['Owner'])
+    print("Name of animal:", pet['Name'])
+
