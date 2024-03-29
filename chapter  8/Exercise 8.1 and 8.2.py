a854158c62757805d129d0136b435c3baf7058a6
Dirac <122928674+dirac271@users.noreply.github.com>
2024-03-29 14:19:15 -0600

    Add files via upload

---
+++ chapter  8/Exercise 8.1 and 8.2.py
@@ -0,0 +1,12 @@
+#Exercise 8.1Message
+def display_message():
+    print("I'm learning fucking python bcz i want to be a hacker, i love u")
+display_message()
+
+#Exercise 8.2 Favorite Book:
+def favorite_book(title):
+    print(f"One of my favorite books is {title.title()}")
+favorite_book('Quantum Mechanics by Paul Dirac')
+
+
+
