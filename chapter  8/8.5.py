4d8958a2b914b71d4df20ac46e8cccfe4af14dd9
Dirac <122928674+dirac271@users.noreply.github.com>
2024-03-30 21:50:51 -0600

    Add files via upload

---
+++ chapter  8/8.5.py
@@ -0,0 +1,6 @@
+#Exercise 8.5
+def describe_city(name_city,name_country='Italy'):
+    print(f"{name_city} is in {name_country}")
+describe_city('Milan')
+describe_city('Roma')
+describe_city('Bristol(incorrect)')
