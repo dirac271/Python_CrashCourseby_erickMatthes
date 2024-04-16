de5a7c7a1911debf31d8fc0bf89b0d922ae5fadd
Dirac <122928674+dirac271@users.noreply.github.com>
2024-04-15 20:37:12 -0600

    Add files via upload

---
+++ chapter  8/Last exercises/printing_models.py
@@ -0,0 +1,6 @@
+import printing_functions
+# Start with some designs that need to be printed.
+unprinted = ['phone case', 'robot pendant', 'dodecahedron']
+completed = []
+printing_functions.print_models(unprinted,completed)
+printing_functions.show_completed_models(completed)
