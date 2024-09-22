f460dda3995ca22d547c3213705e4928029299ce
Dirac <122928674+dirac271@users.noreply.github.com>
2024-09-22 14:11:04 -0600

    Add files via upload

---
+++ Chapter 9/ Importing Classes/WorkCarpet/Imported Restaurant.py
@@ -0,0 +1,4 @@
+from resti import Restaurant
+my_PizzaHut = Restaurant('Pizza hut',"CHESSE CHICKEN")
+
+my_PizzaHut.describe_restaurant()
---
+++ Chapter 9/ Importing Classes/WorkCarpet/resti.py
@@ -0,0 +1,9 @@
+class Restaurant:
+    def __init__(self,restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+    def describe_restaurant(self):
+        print(f"\nThe restaurant {self.restaurant_name} has cuisine {self.cuisine_type}")
+    def open_restaurant(self):
+        print(f"The restaurant {self.restaurant_name} is open!")
+
